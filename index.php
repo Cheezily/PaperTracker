c6ff72ec1af7da7526e6c85f085e4eb49b7c74ca
Cheezily <pm655@yahoo.com>
2016-07-09 15:22:37 -0500

    add additional structure and start cookie management

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
-    include 'static/header.php';
+    
+    if (empty($_SESSION)) {
+        $lifetime = 90000;
+        session_set_cookie_params($lifetime, '/');
+        session_start();
+        $_SESSION['token'] = '9872344234';
+        $_SESSION['last_activity'] = time();
+    }
+
+    session_destroy();
+    
+    echo "Time ".time()."<br>";
+
+    if (!empty($_SESSION) && (time() - $_SESSION['last_activity']) > 30) {
+        echo 'The session has expired.  Please log in again.';
+        $_SESSION = array();
+    } else {
+        include 'static/header.php';
+    }
+    
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- static/header.php
+++ static/header.php
@@ -2,5 +2,8 @@
 
 <header>
     hello there
+    <?php print_r($_SESSION).'<br>';
+        echo $_SESSION['_last_activity'];
+    ?>
 </header>
 
