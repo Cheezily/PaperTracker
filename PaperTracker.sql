8373621b6daa6c84f62b570a8ec798f5146bc188
Cheezily <pm655@yahoo.com>
2016-08-24 19:49:35 -0500

    update database schema

--- PaperTracker.sql
+++ PaperTracker.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 19, 2016 at 11:23 PM
+-- Generation Time: Aug 25, 2016 at 02:45 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.19
 
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `messages` (
+  `messageID` int(12) NOT NULL,
   `fromUsername` varchar(30) NOT NULL,
   `toUsername` varchar(30) NOT NULL,
   `whenSent` datetime NOT NULL,
@@ -43,22 +44,20 @@ CREATE TABLE `messages` (
 CREATE TABLE `papers` (
   `paperID` int(10) NOT NULL,
   `username` varchar(30) NOT NULL,
-  `reviewerID` int(11) DEFAULT NULL,
-  `filename` varchar(50) NOT NULL,
-  `status` enum('awaiting_assignment','awaiting_review','awaiting_revisions','accepted','rejected') NOT NULL DEFAULT 'awaiting_assignment',
-  `reply_filename` varchar(50) DEFAULT NULL,
-  `when_submitted` datetime NOT NULL,
-  `when_revised` datetime DEFAULT NULL,
-  `when_completed` datetime DEFAULT NULL,
+  `reviewername` varchar(30) DEFAULT NULL,
+  `recommendation` enum('accept','minor','major','reject') DEFAULT NULL,
+  `draftFilename` varchar(50) NOT NULL,
+  `firstReplyFilename` varchar(50) DEFAULT NULL,
+  `revisedFilename` varchar(50) DEFAULT NULL,
+  `finalReplyFilename` varchar(50) DEFAULT NULL,
+  `status` enum('awaiting_assignment','awaiting_review','awaiting_revisions','revisions_submitted','accepted','rejected') NOT NULL DEFAULT 'awaiting_assignment',
+  `whenSubmitted` datetime NOT NULL,
+  `whenFirstReply` datetime DEFAULT NULL,
+  `whenRevised` datetime DEFAULT NULL,
+  `whenFinalReply` datetime DEFAULT NULL,
+  `whenCompleted` datetime DEFAULT NULL,
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `papers`
---
-
-INSERT INTO `papers` (`paperID`, `username`, `reviewerID`, `filename`, `status`, `reply_filename`, `when_submitted`, `when_revised`, `when_completed`, `title`) VALUES
-(12, 'user', NULL, 'TEST.docx', 'awaiting_assignment', NULL, '2016-08-19 22:20:43', NULL, NULL, 'This is the first uploaded!');
 
 -- --------------------------------------------------------
 
@@ -84,18 +83,25 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`userID`, `username`, `account_created`, `last_login`, `first_name`, `last_name`, `email`, `passwordHash`, `role`) VALUES
 (1, 'admin', '2016-08-19 00:00:00', '2016-08-19 13:48:30', 'Firstname', 'Lastname', 'admin@email.com', '$2y$10$pEPtxGEQCUWvu6CBfavyIeHxrZnZo.cDaCCkHzqzU7p.uKbESm4tS', 'admin'),
-(2, 'user', '2016-08-19 20:50:58', '2016-08-19 15:20:30', 'firstname', 'lastname', 'user@email.com', '$2y$11$oQ4PS8cdXbhUbxZ9Gz/rLOApcNFwdGe41IZ0x/1zFJyQWINHnOW5u', 'author');
+(3, 'reviewer', '2016-08-20 04:31:38', '2016-08-20 15:14:03', 'firstname', 'lastname', 'rev@wmil.com', '$2y$11$cIqYazL6Z3tEc9AR0ZwQyuDpDSFIlOtBqky1FlbzAryUuJDMW6Hmq', 'reviewer'),
+(5, 'user', '2016-08-21 04:34:55', '2016-08-20 21:34:55', 'first', 'last', 'email@email.com', '$2y$11$fSL8mjGwt/SSWhJKfFMIwuPOAiIqx4Wo6Hi8mTqyfFoZ2MA3vtD/y', 'author');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`messageID`);
+
+--
 -- Indexes for table `papers`
 --
 ALTER TABLE `papers`
   ADD PRIMARY KEY (`paperID`),
-  ADD UNIQUE KEY `filename` (`filename`);
+  ADD UNIQUE KEY `filename` (`draftFilename`);
 
 --
 -- Indexes for table `users`
@@ -109,15 +115,20 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `messageID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
+--
 -- AUTO_INCREMENT for table `papers`
 --
 ALTER TABLE `papers`
-  MODIFY `paperID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `paperID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
