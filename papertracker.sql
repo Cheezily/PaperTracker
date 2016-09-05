e029c55a3f86c0cef14a244f04a0605138fb9d76
cheezily <pm655@yahoo.com>
2016-09-05 18:12:27 -0500

    add db field to allow responses to messages instead of keeping responses separate

--- papertracker.sql
+++ papertracker.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2016 at 05:34 PM
+-- Generation Time: Sep 06, 2016 at 01:10 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -31,7 +31,9 @@ CREATE TABLE `messages` (
   `fromUsername` varchar(30) NOT NULL,
   `toUsername` varchar(30) NOT NULL,
   `whenSent` datetime NOT NULL,
+  `whenReplied` datetime DEFAULT NULL,
   `message` text NOT NULL,
+  `reply` text,
   `title` varchar(255) NOT NULL,
   `newMessage` tinyint(4) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -40,8 +42,12 @@ CREATE TABLE `messages` (
 -- Dumping data for table `messages`
 --
 
-INSERT INTO `messages` (`messageID`, `fromUsername`, `toUsername`, `whenSent`, `message`, `title`, `newMessage`) VALUES
-(105, 'admin', 'user', '2016-08-25 10:14:06', 'message', 'General Question', 1);
+INSERT INTO `messages` (`messageID`, `fromUsername`, `toUsername`, `whenSent`, `whenReplied`, `message`, `reply`, `title`, `newMessage`) VALUES
+(105, 'admin', 'user', '2016-08-25 10:14:06', NULL, 'message', NULL, 'General Question', 1),
+(106, 'user', 'admin', '2016-09-05 11:22:19', NULL, 'This is a test!!!', NULL, 'General Question', 1),
+(107, 'user', 'admin', '2016-09-04 11:30:50', NULL, 'What?? Another message????', NULL, 'General Question', 1),
+(108, 'user', 'admin', '2016-09-05 12:31:02', NULL, 'New message time!', NULL, 'General Question', 1),
+(109, 'user', 'admin', '2016-09-05 12:31:35', NULL, 'Test message', NULL, 'General Question', 1);
 
 -- --------------------------------------------------------
 
@@ -68,6 +74,13 @@ CREATE TABLE `papers` (
   `recentlyUpdated` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `papers`
+--
+
+INSERT INTO `papers` (`paperID`, `username`, `reviewername`, `recommendation`, `draftFilename`, `firstReviewFilename`, `revisedFilename`, `finalReviewFilename`, `status`, `whenSubmitted`, `whenFirstReply`, `whenRevised`, `whenFinalReply`, `whenCompleted`, `title`, `recentlyUpdated`) VALUES
+(1, 'user', NULL, NULL, '3810-TEST--New Microsoft Word Document.docx', NULL, NULL, NULL, 'awaiting_assignment', '2016-09-05 11:39:46', NULL, NULL, NULL, NULL, 'This is a Test!', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -91,9 +104,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userID`, `username`, `account_created`, `last_login`, `first_name`, `last_name`, `email`, `passwordHash`, `role`) VALUES
-(1, 'admin', '2016-08-19 00:00:00', '2016-09-03 10:07:59', 'Firstname', 'Lastname', 'admin@email.com', '$2y$10$pEPtxGEQCUWvu6CBfavyIeHxrZnZo.cDaCCkHzqzU7p.uKbESm4tS', 'admin'),
-(3, 'reviewer', '2016-08-20 04:31:38', '2016-09-02 09:10:57', 'firstname', 'lastname', 'rev@wmil.com', '$2y$11$cIqYazL6Z3tEc9AR0ZwQyuDpDSFIlOtBqky1FlbzAryUuJDMW6Hmq', 'reviewer'),
-(5, 'user', '2016-08-21 04:34:55', '2016-09-03 10:05:15', 'first', 'last', 'email@email.com', '$2y$11$fSL8mjGwt/SSWhJKfFMIwuPOAiIqx4Wo6Hi8mTqyfFoZ2MA3vtD/y', 'author'),
+(1, 'admin', '2016-08-19 00:00:00', '2016-09-05 12:31:40', 'Firstname', 'Lastname', 'admin@email.com', '$2y$10$pEPtxGEQCUWvu6CBfavyIeHxrZnZo.cDaCCkHzqzU7p.uKbESm4tS', 'admin'),
+(3, 'reviewer', '2016-08-20 04:31:38', '2016-09-05 10:52:52', 'firstname', 'lastname', 'rev@wmil.com', '$2y$11$cIqYazL6Z3tEc9AR0ZwQyuDpDSFIlOtBqky1FlbzAryUuJDMW6Hmq', 'reviewer'),
+(5, 'user', '2016-08-21 04:34:55', '2016-09-05 11:22:05', 'first', 'last', 'email@email.com', '$2y$11$fSL8mjGwt/SSWhJKfFMIwuPOAiIqx4Wo6Hi8mTqyfFoZ2MA3vtD/y', 'author'),
 (6, 'newuser', '2016-08-31 01:11:11', '2016-08-30 19:32:22', 'first', 'last', 'email@email.com', '$2y$11$tIcWiltjNU5Xi.SUVcN06.DaFytYq7e/LRMhgU3GoMxnfsNKJd9mO', 'author');
 
 --
@@ -128,12 +141,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `messageID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+  MODIFY `messageID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 --
 -- AUTO_INCREMENT for table `papers`
 --
 ALTER TABLE `papers`
-  MODIFY `paperID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `paperID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
